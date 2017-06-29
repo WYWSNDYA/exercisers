756884616e8d660554a1233388fe6f5b1fccfd7e
闻飞 <780389404@qq.com>
2017-06-29 20:56:18 +0800

    这是学习的第二个过程

--- test1/ViewController.m
+++ test1/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
+#define iOS8 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
 typedef enum //定义一个常见的枚举类型
 {
     
@@ -17,29 +19,100 @@ typedef enum //定义一个常见的枚举类型
     
 }assert;//枚举名字可直接在项目中应用这个枚举的类型值并将其应用到想要的类型中去完成这个类型的应用值类型
 
+@interface ViewController()
 
-@interface ViewController ()
+@end//扩展这个类的一系列的属性以及方法
 
-@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    int c = playa+playb;
-    NSLog(@"%d",c);
-    UIButton * oneButton =[[UIButton alloc]init];
+   
+    /*WKWebView *webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.baidu.com"]]];
+    [self.view addSubview:webView];*/
     
-    oneButton.backgroundColor = [UIColor redColor];
+    WKWebView * wkWebView =[[WKWebView alloc]initWithFrame:self.view.bounds];
     
-    oneButton.frame = CGRectMake(20,20, 40, 40);
+    wkWebView.UIDelegate=self;
+    
+    wkWebView.navigationDelegate = self;
+    
+    [wkWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
+    
+    [self.view addSubview:wkWebView];//去使用wkwebview代替webview
     
     
-    [self.view addSubview:oneButton];
+    NSLog(@">>>%f",[[[UIDevice currentDevice]systemVersion]floatValue]);
+    
+    
+    if (iOS8) {
+        NSLog(@"ios8之后的操作");
+    }else{
+        
+        NSLog(@"ios8之前的操作");
+        
+    }
+    
+    if ([self is64bit]) {
+        NSLog(@"这是一位64的操作系统");
+    }else
+    {
+        
+        NSLog(@"nitm的又在骗我去死吧 sb");
+        
+        
+    }
+   
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 
+
+
+
+- (BOOL)is64bit
+{
+    
+#if defined(__LP64__) && __LP64__
+    
+    return YES;
+    
+    
+#else
+    
+    return NO;
+    
+#endif
+    
+}
+
+-(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation
+{
+    
+    NSLog(@"wwwwwww");
+    
+}
+
+// 当内容开始返回时调用
+-(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+    
+    
+    
+}
+// 页面加载完成之后调用
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    
+     NSLog(@"这是一个加载成功的展示效果");
+    
+}
+// 页面加载失败时调用
+-(void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    
+
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
